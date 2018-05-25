bcd33dde3bb541b4afbdb2de8f831a463496f986
Matthew Beshara <mbeshara@users.noreply.github.com>
2018-05-25 13:35:34 -0400

    Add UrbanCode standards (#1)
    * Remove duplicate build.gradle content
    
    * Remove duplicate maven repo
    
    * Remove groovy-plugin-util classes
    
    * Replace hardcoded release version with token
    
    * Add current copyright lines
    
    * Add .gitignore
    
    * Remove execute bit from files
    
    * Set unix line endings
    
    * Fix history in README
    
    * Add release notes in info.xml
    
    * Roll back to version 20
    
    * Add version 20 to upgrade.xml

--- src/main/groovy/com/urbancode/air/Venafi/VenafiHelper.groovy
+++ src/main/groovy/com/urbancode/air/Venafi/VenafiHelper.groovy
@@ -1,7 +1,7 @@
 /*
  * Licensed Materials - Property of IBM Corp.
  * IBM UrbanCode Deploy
- * (c) Copyright IBM Corporation 2011, 2014. All Rights Reserved.
+ * (c) Copyright IBM Corporation 2017. All Rights Reserved.
  *
  * U.S. Government Users Restricted Rights - Use, duplication or disclosure restricted by
  * GSA ADP Schedule Contract with IBM Corp.
